a32b10891f984f7642e525d36f9be57e6f7374ab
unknown <agnieszkaperchuc@gmail.com>
2018-06-04 18:36:06 +0200

    add contractor entity with CRUD

---
+++ src/MMBundle/Entity/Contractor.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace MMBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Contractor
+ *
+ * @ORM\Table(name="contractor")
+ * @ORM\Entity(repositoryClass="MMBundle\Repository\ContractorRepository")
+ */
+class Contractor
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Contractor
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
+
